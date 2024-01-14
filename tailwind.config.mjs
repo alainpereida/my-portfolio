7d93b9212518993f5f9c2002d829535c85cdd822
Alain Pereida Prado <alain.pereida@gmail.com>
2024-01-13 17:40:40 -0800

    style: correcting text about my Experience

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 const baseColors = {
-	primary: {
-	  dark: '#000308',
-	  light: '#fff',
-	},
-	secondary: {
-	  dark: '#008D54',
-	  light: '#00db82',
-  }
-}
+  primary: {
+    dark: "#000308",
+    light: "#fff",
+  },
+  secondary: {
+    dark: "#008D54",
+    light: "#00db82",
+  },
+  emerald: {
+    50: "#edfff7",
+    100: "#d5ffee",
+    200: "#aeffde",
+    300: "#70ffc6",
+    400: "#2bfda7",
+    500: "#00db82",
+    600: "#00c06d",
+    700: "#009659",
+    800: "#067549",
+    900: "#07603e",
+    950: "#003721",
+  },
+};
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				// ...baseColors,
-				primary: baseColors.primary.light,
-				secondary: baseColors.secondary.light,
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        // ...baseColors,
+        primary: baseColors.primary.light,
+        secondary: baseColors.secondary.light,
+      },
+    },
+  },
+  plugins: [],
+};
